766d382c6b6b2f3628a4b849e6e0ae3356b4ae68
anampict <anampict@gmail.com>
2025-07-21 12:44:52 +0700

    set up retrofit

--- app/src/main/java/com/muhib/galonku/data/model/User.kt
+++ app/src/main/java/com/muhib/galonku/data/model/User.kt
@@ -1,2 +1,10 @@
 package com.muhib.galonku.data.model
 
+data class User(
+    val _id: String,
+    val name: String,
+    val email: String,
+    val password: String? = null,
+    val role: String = "user", // default user/admin
+
+)
--- app/src/main/java/com/muhib/galonku/data/network/ApiClient.kt
+++ app/src/main/java/com/muhib/galonku/data/network/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object ApiClient {
-    private const val BASE_URL = " "
+    private const val BASE_URL = "http://192.168.100.60:8080/"
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
