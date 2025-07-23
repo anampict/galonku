5d338a191ed00a5bf2a2555092db21ba4a0855c1
anampict <anampict@gmail.com>
2025-07-24 00:25:53 +0700

    desain halaman login

---
+++ app/src/main/java/com/muhib/galonku/commond/Font.kt
@@ -0,0 +1,14 @@
+package com.muhib.galonku.commond
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.muhib.galonku.R
+
+val poppinsfamily = FontFamily(
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+
+    )
--- app/src/main/java/com/muhib/galonku/pages/auth/login/LoginScreen.kt
+++ app/src/main/java/com/muhib/galonku/pages/auth/login/LoginScreen.kt
@@ -1,14 +1,18 @@
 package com.muhib.galonku.pages.auth.login
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
@@ -24,21 +28,28 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.muhib.galonku.AppScreen
+import com.muhib.galonku.R
+import com.muhib.galonku.commond.poppinsfamily
 import com.muhib.galonku.data.model.User
 import com.muhib.galonku.data.viewmodel.AuthViewModel
 
 @Composable
 fun LoginScreen(
     navController: NavController,
-   viewModel: AuthViewModel
+    viewModel: AuthViewModel
 ) {
     val context = LocalContext.current
     var email by remember { mutableStateOf("") }
@@ -65,61 +76,93 @@ fun LoginScreen(
         }
     }
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center,
+            .background(color = Color(0xffE4EAF9))
+            .padding(start = 26.dp, end = 26.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Login", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        item {
+            Spacer(Modifier.height(65.dp))
+            Image(
+                painter = painterResource(id = R.drawable.login),
+                contentDescription = "login",
+                contentScale = ContentScale.FillBounds
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            singleLine = true,
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                val user = User(email = email, password = password)
-                viewModel.login(user)
-            },
-            enabled = !isLoading,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            if (isLoading) {
-                CircularProgressIndicator(
-                    modifier = Modifier.size(20.dp),
-                    strokeWidth = 2.dp,
-                    color = Color.White
+            Row(modifier = Modifier.fillMaxWidth().padding(start = 8.dp, top = 59.dp)) {
+                Text(
+                    "Login",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = poppinsfamily,
+                    color = Color(0xff6E6E6E)
                 )
-            } else {
-                Text("Login")
+
+            }
+
+            Text(
+                "Silahkan Login Ke Akun Anda",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = poppinsfamily,
+                color = Color(0xff6E6E6E)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    val user = User(email = email, password = password)
+                    viewModel.login(user)
+                },
+                enabled = !isLoading,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                if (isLoading) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(20.dp),
+                        strokeWidth = 2.dp,
+                        color = Color.White
+                    )
+                } else {
+                    Text("Login")
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            TextButton(onClick = { navController.navigate(AppScreen.Register.route) }) {
+                Text("Belum punya akun? Daftar")
             }
         }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextButton(onClick = { navController.navigate(AppScreen.Register.route) }) {
-            Text("Belum punya akun? Daftar")
-        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen(navController = rememberNavController(), viewModel = viewModel())
 }
